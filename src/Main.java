13602726c66f8cf85320f0f5b3620a34e34c21b3
Ishan854 <srivastava.ishan854@gmail.com>
2023-11-17 13:27:12 +0530

    Modified Code

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -22,7 +24,7 @@ public class Main {
 
 
         WebDriverManager.chromedriver().setup();
-        WebDriver driver =  new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://telugu.timesnownews.com/");
         String currentURL = driver.getCurrentUrl();
@@ -31,39 +33,48 @@ public class Main {
         System.out.println("Page Source = " + pageSource);
         String title = driver.getTitle();
         System.out.println("Title =  " + title);
-        List<WebElement> total_Links = driver.findElements(By.tagName("link"));
+        List<WebElement> total_Links = driver.findElements(By.tagName("a"));
         int count = total_Links.size();
         System.out.println("Number of links available on this Website: " + count);
         List<String> linkList = new ArrayList<>();
         List<String> not200Links = new ArrayList<>();
-        Map<String, Integer> linkStatus   = new HashMap<>();
+        Map<String, Integer> linkStatus = new HashMap<>();
         for (int i = 0; i < count; i++) {
             String link = total_Links.get(i).getAttribute("href");
             linkList.add(link);
         }
-        for (String link : linkList) {
-            System.out.println(link);
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(new URI(link))
-                    .timeout(Duration.of(10, ChronoUnit.SECONDS))
-                    .GET()
-                    .build();
-            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-            int statusCode = response.statusCode();
-            System.out.println("Status Code: " + statusCode);
-            System.out.println("Headers" + response.headers());
-            linkStatus.put(link, statusCode);
 
-        }
-        if(linkStatus.size() > 0){
-            System.out.println("Links and Status Code");
-            for(Map.Entry<String, Integer> entry: linkStatus.entrySet()){
-                System.out.println("Links: " + entry.getKey() + ", Status Code: " + entry.getValue());
+        try (FileWriter fileWriter = new FileWriter("LinkStatus.csv")) {
+            for (String link : linkList) {
+                System.out.println(link);
+
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(new URI(link))
+                        .timeout(Duration.of(10, ChronoUnit.SECONDS))
+                        .GET()
+                        .build();
+
+                HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+                int statusCode = response.statusCode();
+
+                System.out.println("Status Code: " + statusCode);
+                System.out.println("Headers" + response.headers());
+                linkStatus.put(link, statusCode);
+
+                fileWriter.write(link + "\t" + statusCode + "\n");
             }
-        }else{
+        }
+        if (linkStatus.size() > 0) {
+            System.out.println("Links and Status Code");
+            for (Map.Entry<String, Integer> entry : linkStatus.entrySet()) {
+                System.out.println("Links: " + entry.getKey() + ", Status Code: " + entry.getValue());
+
+            }
+
+        } else {
             System.out.println("All Links Have 200 Status Code");
         }
 
-    driver.quit();
+        driver.quit();
     }
 }
